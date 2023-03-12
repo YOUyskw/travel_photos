bf3fe542f0eaa0639510fe91bb465ea7f9872b59
YukiKamo <yukikamo@MacBook-Pro-2.local>
2023-03-12 17:33:55 +0900

    カメラ動いたけど型エラーが消えない

--- src/app/group/[groupId]/camera/page.tsx
+++ src/app/group/[groupId]/camera/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+import Image from "next/image";
+import React, { useRef, useState } from "react";
+import { Camera, CameraProps } from "react-camera-pro";
+
 export default function Page() {
-  return <h1>camera</h1>;
+  const camera =
+    useRef<
+      React.ForwardRefExoticComponent<
+        CameraProps & React.RefAttributes<unknown>
+      >
+    >(null);
+  const [image, setImage] = useState("");
+  return (
+    <>
+      <Camera
+        ref={camera}
+        errorMessages={{
+          noCameraAccessible: undefined,
+          permissionDenied: undefined,
+          switchCamera: undefined,
+          canvas: undefined,
+        }}
+      />
+      <button onClick={() => setImage(camera.current?.takePhoto())}>
+        Take photo
+      </button>
+      <Image src={image} alt="Taken photo" />
+    </>
+  );
 }
