7e8ed93a9de3aeb9d03ae675139ba28f23ebe143
hiranorrr <hiuni_mono100@me.com>
2023-03-14 16:18:25 +0900

    ボーダーラインの所属変更

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 export default async function Page() {
   return (
     <>
-      <h1 className="pt-10 pb-2 font-bold border-b-2 mx-2">
-        過去のグループ一覧
-      </h1>
-      <h2 className="mx-2 pt-2 font-bold">グループA</h2>
+      <h1 className="pt-10 pb-2 font-bold mx-2">過去のグループ一覧</h1>
+      <h2 className="mx-2 pt-2 font-bold border-t-2">グループA</h2>
       <div className="overflow-x-scroll flex">
         <Image
           src="/IMG_4422.jpeg"
