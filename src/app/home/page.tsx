ef4c6011d5c9214d417b10edcf50229cd6d750b9
hiranorrr <hiuni_mono100@me.com>
2023-03-14 17:00:36 +0900

    文字重ねる

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -11,13 +11,18 @@ export default async function Page() {
       <h1 className="pt-10 pb-2 font-bold mx-2">過去のグループ一覧</h1>
       <h2 className="mx-2 pt-2 font-bold border-t-2">グループA</h2>
       <div className="overflow-x-scroll flex">
-        <Image
-          src="/IMG_4422.jpeg"
-          alt="not found"
-          width="414"
-          height="896"
-          className="p-4 rounded-3xl"
-        />
+        <div className="relative shrink-0">
+          <Image
+            src="/IMG_4422.jpeg"
+            alt="not found"
+            width="414"
+            height="896"
+            className="rounded-3xl"
+          />
+          <div className="absolute top-6 left-6 text-4xl w-full right-6">
+            11月2日
+          </div>
+        </div>
         <Image
           src="/IMG_4422.jpeg"
           alt="not found"
