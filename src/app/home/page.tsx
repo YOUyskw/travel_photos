03b49cc8d779401071ffc84b054fbccae036876e
hiranorrr <hiuni_mono100@me.com>
2023-03-14 15:31:48 +0900

    画像の表示

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,12 @@
-export default function Page() {
+import Image from "next/image";
+export default async function Page() {
   return (
     <>
-      <h1 className="pt-20 pb-2 font-bold border-b-2 mx-2">
+      <h1 className="pt-10 pb-2 font-bold border-b-2 mx-2">
         過去のグループ一覧
       </h1>
       <h2 className="mx-2 pt-2 font-bold">グループA</h2>
-      <input />
+      <Image src="/IMG_4422.jpeg" alt="not found" width="414" height="896" />
     </>
   );
 }
