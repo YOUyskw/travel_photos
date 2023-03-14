7da55ed5c90a43c27a0a5abcf3c19356372d7835
YukiKamo <yukikamo@MacBook-Pro-2.local>
2023-03-14 16:51:22 +0900

    iphone的なボタンからデータ取得

--- src/app/group/[groupId]/camera/page.tsx
+++ src/app/group/[groupId]/camera/page.tsx
@@ -7,8 +7,9 @@ export default function Page() {
   const camera = useRef<CameraType>(null);
   const [image, setImage] = useState<string>("");
   return (
-    <div>
+    <div className="bg-black h-screen">
       <Camera
+        aspectRatio={9 / 16}
         ref={camera}
         errorMessages={{
           noCameraAccessible: undefined,
@@ -17,7 +18,8 @@ export default function Page() {
           canvas: undefined,
         }}
       />
-      <div className="fixed flex justify-center bg-black bottom-0 w-full">
+      <div className="fixed flex justify-center items-center bg-black bottom-0 w-full">
+        <p className="left-0.5 text-white basis-1/4 mr-3">キャンセル</p>
         <div className="w-24 h-24 z-20 rounded-full border border-black bg-white flex justify-center my-2">
           <button
             onClick={() => {
@@ -28,14 +30,8 @@ export default function Page() {
             className="z-20 h-20 w-20 rounded-full border-black border-2 bg-white my-auto"
           />
         </div>
+        <div className="basis-1/4" />
       </div>
-      <Image
-        className="z-20"
-        src={image}
-        alt="Taken photo"
-        width={20}
-        height={20}
-      />
     </div>
   );
 }
