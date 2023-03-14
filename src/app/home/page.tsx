27f1689995d2f5b275042b2f760fe4069cc16fcc
hiranorrr <hiuni_mono100@me.com>
2023-03-14 14:45:42 +0900

    ほぼ見出しだけ

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <h1 className="p-8">home</h1>;
+  return (
+    <>
+      <h1 className="pt-20 pb-2 font-bold border-b-2 mx-2">
+        過去のグループ一覧
+      </h1>
+      <h2 className="mx-2 pt-2 font-bold">グループA</h2>
+      <input />
+    </>
+  );
 }
