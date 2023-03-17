b4cc292ed41eab0d20513a6ea971763aaabc2f65
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-03-18 02:20:16 +0900

    feat: album page api connection

--- src/app/group/[groupId]/[albumId]/page.tsx
+++ src/app/group/[groupId]/[albumId]/page.tsx
@@ -3,6 +3,18 @@ import { getAlbum } from "@/repo/album";
 import Image from "next/image";
 import Link from "next/link";
 
+// 日付フォーマット
+function DateTransformer(date: Date) {
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formattedDate = `${month}/${day} ${hours < 10 ? "0" : ""}${hours}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+  return formattedDate;
+}
+
 type PageProps = {
   params: { groupId: string; albumId: string };
 };
@@ -10,7 +22,7 @@ type PageProps = {
 export default async function Page({
   params: { groupId, albumId },
 }: PageProps) {
-  const alubm = await getAlbum(groupId, albumId);
+  const album = await getAlbum(groupId, albumId);
 
   return (
     <>
@@ -18,15 +30,17 @@ export default async function Page({
       <div className="mt-20">
         <main className="m-4">
           <div className="my-5">
-            <p className="font-bold">北九州市小倉北区馬借 付近</p>
-            <p className="text-zinc-400">3/10 18:00 ・ 36枚の写真</p>
+            <p className="font-bold">{album[0].location_name}</p>
+            <p className="text-zinc-400">
+              {DateTransformer(album[0].createdAt)}・{album.length}枚の写真
+            </p>
           </div>
           <div className="grid grid-cols-3 gap-1">
-            {alubm.map((photo) => (
+            {album.map((photo) => (
               <Link key={photo.id} href={`/group/${groupId}/photo/${photo.id}`}>
                 <div className="relative aspect-square" key={photo.id}>
                   <Image
-                    src={photo.url}
+                    src={photo.downloadUrl}
                     alt="cute dog"
                     className="block object-cover w-48 h-48 bg-gray-100"
                     fill
