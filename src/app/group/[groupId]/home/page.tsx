d901510238d6d8d4cb6fccd582d806f2a79caa09
yu0ki <49509576+yu0ki@users.noreply.github.com>
2023-03-17 14:44:25 +0900

    [add] firebaseからデータ取得

--- src/app/group/[groupId]/home/page.tsx
+++ src/app/group/[groupId]/home/page.tsx
@@ -49,123 +49,43 @@ export default async function Page({ params: { groupId } }: PageProps) {
           style={{ width: `calc(100% + 48px)`, transform: "translateX(-24px)" }}
         />
 
+        {/* mapでループさせる */}
         {/* 時間ごとの塊 */}
-        <div className="px-5  bg-[url('../../public/timeline_border.png')] bg-cover mt-5">
-          {/* 場所と時間 */}
-          <div className="py-1 mx-10">
-            <p className="text-zinc-400">3/10 18:00</p>
-            <p className="">北九州市小倉北区馬借 付近</p>
-            <a href="./2" className="absolute top-5 right-0">
-                <AiOutlineArrowRight />
-              </a>
-          </div>
-          {/* 写真 */}
-          <div className="flex py-5 ml-5 overflow-auto flex-nowrap">
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                alt="cute dog"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
+        {DUMMY_ALBUMS.map((segment_album) => 
+          <div className="px-5  bg-[url('../../public/timeline_border.png')] bg-cover mt-5">
+            {/* 場所と時間 */}
+            <div className="py-1 mx-10">
+              {/* とりあえず1枚目の画像の情報を流用 */}
 
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://image.hldy-cdn.com/c/w=1336,h=826,g=5,a=2,r=auto,f=webp:auto/holiday_article_images/4709/4709.jpg?1592537784"
-                alt="kiyomizu"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
-
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://rimage.gnst.jp/livejapan.com/public/article/detail/a/20/00/a2000423/img/basic/a2000423_main.jpg?20201203161217&q=80"
-                alt="kyoto"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
-
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://photo-map.net/wp-content/uploads/2019/10/s_shutterstock_1402684670.jpg"
-                alt="kyoto"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
-
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://resize2-icotto.k-img.com/ikCOVnqBypZZTR7HaWd9ZOgmktSji7OvZeF0UJlrCtI/rs:fill:700:467/plain/https://icotto.k-img.com/system/press_images/001/000/856/86999f0644e4cff69a163f96666828538b3486f2.jpg"
-                alt="kyoto"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* 時間ごとの塊 */}
-        <div className="px-5  bg-[url('../../public/timeline_border.png')] bg-cover">
-          {/* 場所と時間 */}
-           <div className="py-1 mx-10 relative">
-              <p className="text-zinc-500">日付 + 時間</p>
-              <p className="text-2xl font-bold">行った場所 + 付近</p>
+              <p className="text-zinc-400">{segment_album[0].date}</p>
+              <p className="">{segment_album[0].location} 付近</p>
               <a href="./2" className="absolute top-5 right-0">
-                <AiOutlineArrowRight />
+                  <AiOutlineArrowRight />
               </a>
             </div>
-          {/* 写真 */}
-          <div className="flex py-5 ml-5 overflow-auto flex-nowrap">
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                alt="cute dog"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
+            
+            
+            
 
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://image.hldy-cdn.com/c/w=1336,h=826,g=5,a=2,r=auto,f=webp:auto/holiday_article_images/4709/4709.jpg?1592537784"
-                alt="kiyomizu"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
-
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://rimage.gnst.jp/livejapan.com/public/article/detail/a/20/00/a2000423/img/basic/a2000423_main.jpg?20201203161217&q=80"
-                alt="kyoto"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
-
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://photo-map.net/wp-content/uploads/2019/10/s_shutterstock_1402684670.jpg"
-                alt="kyoto"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
-            </div>
-
-            <div className="mx-5 mb-5 shrink-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src="https://resize2-icotto.k-img.com/ikCOVnqBypZZTR7HaWd9ZOgmktSji7OvZeF0UJlrCtI/rs:fill:700:467/plain/https://icotto.k-img.com/system/press_images/001/000/856/86999f0644e4cff69a163f96666828538b3486f2.jpg"
-                alt="kyoto"
-                className="block object-cover w-64 h-48 rounded-md"
-              />
+            <div className="flex py-5 ml-5 overflow-auto flex-nowrap">
+              {segment_album.map(
+                (photo) =>
+                
+                <div className="mx-5 mb-5 shrink-0">
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src={photo.url}
+                    alt={photo.location}
+                    className="block object-cover w-64 h-48 rounded-md"
+                  />
+                </div>
+              )}
             </div>
           </div>
-        </div>
+          )}
+        
+
+        
 
         {/* カメラへのリンク */}
         <div className="sticky bottom-0 text-white">
