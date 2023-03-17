f828bf0ef4ff28e7edc4359215f0ef85dc90ce5c
YukiKamo <yukikamo@MacBook-Pro-2.local>
2023-03-17 18:00:10 +0900

    上から32pxでアラート出す

--- src/app/group/[groupId]/home/page.tsx
+++ src/app/group/[groupId]/home/page.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/Header";
 import Image from "next/image";
 import { getGroup } from "@/repo/group";
-import { DUMMY_ALBUMS } from "@/repo/dummy"
+import { DUMMY_ALBUMS } from "@/repo/dummy";
 import { FiShare } from "react-icons/fi";
-import { AiOutlineArrowRight } from "react-icons/ai"
-import { AiOutlineCamera } from "react-icons/ai"
-import Link from 'next/link';
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineCamera } from "react-icons/ai";
+import Link from "next/link";
 
 type PageProps = {
   params: { groupId: string };
@@ -18,19 +18,23 @@ function DateTransformer(date_string: string) {
   const day = date.getDate();
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  const formattedDate = `${month < 10 ? "0" : ""}${month}/${day < 10 ? "0" : ""}${day} ${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
-  return formattedDate
+  const formattedDate = `${month < 10 ? "0" : ""}${month}/${
+    day < 10 ? "0" : ""
+  }${day} ${hours < 10 ? "0" : ""}${hours}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+  return formattedDate;
 }
 
 // 写真の総枚数カウント
-function countPhotos(albums: any[]){
+function countPhotos(albums: any[]) {
   let count = 0;
-  for (var i = 0; i < albums.length; i++){
-    for (var j = 0; j < albums[i].length; j++){
+  for (var i = 0; i < albums.length; i++) {
+    for (var j = 0; j < albums[i].length; j++) {
       count++;
     }
   }
-  return count
+  return count;
 }
 
 export default async function Page({ params: { groupId } }: PageProps) {
@@ -44,7 +48,8 @@ export default async function Page({ params: { groupId } }: PageProps) {
           <div>
             <p className="text-2xl font-bold">{group.name}</p>
             <p className="text-zinc-400">
-              {countPhotos(DUMMY_ALBUMS)}枚の写真・{group.users.length}人のメンバー
+              {countPhotos(DUMMY_ALBUMS)}枚の写真・{group.users.length}
+              人のメンバー
             </p>
           </div>
           <div className="absolute top-5 right-5">
@@ -74,24 +79,29 @@ export default async function Page({ params: { groupId } }: PageProps) {
 
         {/* mapでループさせる */}
         {/* 時間ごとの塊 */}
-        {DUMMY_ALBUMS.map((segment_album, index) => 
-          <div key={index} className="px-2  bg-[url('../../public/timeline_border2.png')] bg-cover">
+        {DUMMY_ALBUMS.map((segment_album, index) => (
+          <div
+            key={index}
+            className="px-2  bg-[url('../../public/timeline_border2.png')] bg-cover"
+          >
             {/* 場所と時間 */}
             <div className="py-1 mx-10 relative">
               {/* とりあえず1枚目の画像の情報を流用 */}
 
-              <p className="text-zinc-400">{DateTransformer(segment_album[0].date)}</p>
+              <p className="text-zinc-400">
+                {DateTransformer(segment_album[0].date)}
+              </p>
               <p className="">{segment_album[0].location} 付近</p>
-              <Link href={`./group/${groupId}/${index}`} className="absolute top-5 right-2">
-                  <AiOutlineArrowRight />
+              <Link
+                href={`./group/${groupId}/${index}`}
+                className="absolute top-5 right-2"
+              >
+                <AiOutlineArrowRight />
               </Link>
             </div>
-            
 
             <div className="flex py-5 ml-5 overflow-auto flex-nowrap">
-              {segment_album.map(
-                (photo, photo_index) =>
-                
+              {segment_album.map((photo, photo_index) => (
                 <div key={photo_index} className="mx-2 mb-5 shrink-0">
                   <Image
                     src={photo.url}
@@ -101,17 +111,19 @@ export default async function Page({ params: { groupId } }: PageProps) {
                     className="block object-cover w-64 h-48 rounded-md"
                   />
                 </div>
-              )}
+              ))}
             </div>
           </div>
-          )}
-        
-
-        
+        ))}
 
         {/* カメラへのリンク */}
         <div className="sticky bottom-0 text-white">
-          <Link href={`/group/${groupId}/camera`} className="text-5xl absolute bottom-5 right-5 rounded-full border-orange-500 border p-2 bg-orange-500"><AiOutlineCamera /></Link>
+          <Link
+            href={`/group/${groupId}/camera`}
+            className="text-5xl absolute bottom-5 right-5 rounded-full border-orange-500 border p-2 bg-orange-500"
+          >
+            <AiOutlineCamera />
+          </Link>
         </div>
       </main>
     </>
